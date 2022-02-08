73939d3d99366a95dfd08d190e9cdf6264ccdbaa
bhagyashree201 <shreyasha786@gmail.com>
2022-02-09 03:30:40 +0530

    final changes

--- main.js
+++ main.js
@@ -21,3 +21,16 @@ jQuery(document).ready(function($) {
         }
     });
 });
+
+const hamburger=document.querySelector(".hamburger");
+const navLinks=document.querySelector(".navlinks");
+const links=document.querySelectorAll(".navlinks li");
+
+hamburger.addEventListener('click',()=>{
+    navLinks.classList.toggle("open");
+    links.forEach(link=>{
+        link.classList.toggle("fade");
+    });
+
+    hamburger.classList.toggle("toggle");
+});
