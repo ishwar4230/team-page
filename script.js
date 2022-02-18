42e95ba14f7906a4769073ca76a364c7550e4623
ishwar4230 <atcikss9@kgpian.iitkgp.ac.in>
2022-02-18 09:43:43 +0530

    new nav and footer

---
+++ script.js
@@ -0,0 +1,12 @@
+// const hamburger=document.querySelector(".hamburger");
+// const navLinks=document.querySelector(".navlinks");
+// const links=document.querySelectorAll(".navlinks li");
+
+// hamburger.addEventListener('click',()=>{
+//     navLinks.classList.toggle("open");
+//     links.forEach(link=>{
+//         link.classList.toggle("fade");
+//     });
+
+//     hamburger.classList.toggle("toggle");
+// });
